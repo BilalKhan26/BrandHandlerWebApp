092bbead10b34c8dc669fd55453a6cf66480481f
Bilal_k <bilalshafique26@gmail.com>
2025-07-30 21:43:15 +0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrandHandlerWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
